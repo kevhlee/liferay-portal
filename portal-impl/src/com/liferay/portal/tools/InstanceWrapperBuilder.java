40b8e102512f3c39ebb6bcea42785b122d3fc561
Tina Tian <tina.tian@liferay.com>
2024-05-16 14:09:15 -0700

    LPD-21727 SF, inline the only usage, no logic change

--- portal-impl/src/com/liferay/portal/tools/InstanceWrapperBuilder.java
+++ portal-impl/src/com/liferay/portal/tools/InstanceWrapperBuilder.java
@@ -10,7 +10,10 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.xml.Document;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.kernel.xml.UnsecureSAXReaderUtil;
@@ -24,11 +27,26 @@ import com.thoughtworks.qdox.model.JavaParameter;
 import com.thoughtworks.qdox.model.Type;
 import com.thoughtworks.qdox.model.TypeVariable;
 
+import de.hunsicker.io.FileFormat;
+import de.hunsicker.jalopy.Jalopy;
+import de.hunsicker.jalopy.storage.Convention;
+import de.hunsicker.jalopy.storage.ConventionKeys;
+import de.hunsicker.jalopy.storage.Environment;
+
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 
+import java.net.URL;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -237,7 +255,7 @@ public class InstanceWrapperBuilder {
 				StringUtil.replace(javaPackage.getName(), '.', '/'), "/",
 				javaClass.getName(), "_IW.java"));
 
-		ToolsUtil.writeFile(
+		_writeFile(
 			file, sb.toString(), null, ToolsUtil.AUTHOR, null, null, null);
 	}
 
@@ -292,6 +310,159 @@ public class InstanceWrapperBuilder {
 		sb.append(_getDimensions(type));
 
 		return sb.toString();
+	}
+
+	private URL _readJalopyXmlFromClassLoader() {
+		ClassLoader classLoader = ToolsUtil.class.getClassLoader();
+
+		URL url = classLoader.getResource("jalopy.xml");
+
+		if (url == null) {
+			throw new RuntimeException(
+				"Unable to load jalopy.xml from the class loader");
+		}
+
+		return url;
+	}
+
+	private void _write(File file, String s) throws IOException {
+		Path path = file.toPath();
+
+		Files.createDirectories(path.getParent());
+
+		Files.write(path, s.getBytes(StandardCharsets.UTF_8));
+	}
+
+	private void _writeFile(
+			File file, String content, String header, String author,
+			Map<String, Object> jalopySettings, Set<String> modifiedFileNames,
+			String packagePath)
+		throws IOException {
+
+		if (!file.exists()) {
+			_write(file, StringPool.BLANK);
+		}
+
+		if (Validator.isNull(packagePath)) {
+			packagePath = ToolsUtil.getPackagePath(file);
+		}
+
+		String className = file.getName();
+
+		className = className.substring(0, className.length() - 5);
+
+		ImportsFormatter importsFormatter = new JavaImportsFormatter();
+
+		content = importsFormatter.format(content, packagePath, className);
+
+		// Beautify
+
+		StringBuffer sb = new StringBuffer();
+
+		Jalopy jalopy = new Jalopy();
+
+		jalopy.setFileFormat(FileFormat.UNIX);
+		jalopy.setInput(
+			new ByteArrayInputStream(content.getBytes()), file.getPath());
+		jalopy.setOutput(sb);
+
+		File jalopyXmlFile = new File("tools/jalopy.xml");
+
+		if (!jalopyXmlFile.exists()) {
+			jalopyXmlFile = new File("../tools/jalopy.xml");
+		}
+
+		if (!jalopyXmlFile.exists()) {
+			jalopyXmlFile = new File("misc/jalopy.xml");
+		}
+
+		if (!jalopyXmlFile.exists()) {
+			jalopyXmlFile = new File("../misc/jalopy.xml");
+		}
+
+		if (!jalopyXmlFile.exists()) {
+			jalopyXmlFile = new File("../../misc/jalopy.xml");
+		}
+
+		if (jalopyXmlFile.exists()) {
+			Jalopy.setConvention(jalopyXmlFile);
+		}
+		else {
+			URL url = _readJalopyXmlFromClassLoader();
+
+			Jalopy.setConvention(url);
+		}
+
+		if (jalopySettings == null) {
+			jalopySettings = new HashMap<>();
+		}
+
+		Environment env = Environment.getInstance();
+
+		// Author
+
+		author = GetterUtil.getString(
+			(String)jalopySettings.get("author"), author);
+
+		env.set("author", author);
+
+		// Fail on format error
+
+		boolean failOnFormatError = MapUtil.getBoolean(
+			jalopySettings, "failOnFormatError");
+
+		// File name
+
+		env.set("fileName", file.getName());
+
+		Convention convention = Convention.getInstance();
+
+		if (Validator.isNotNull(header)) {
+			convention.put(ConventionKeys.HEADER_TEXT, header);
+		}
+
+		String classMask = "/**\n * @author $author$\n*/";
+
+		convention.put(
+			ConventionKeys.COMMENT_JAVADOC_TEMPLATE_CLASS,
+			env.interpolate(classMask));
+
+		convention.put(
+			ConventionKeys.COMMENT_JAVADOC_TEMPLATE_INTERFACE,
+			env.interpolate(classMask));
+
+		boolean formatSuccess = jalopy.format();
+
+		String newContent = sb.toString();
+
+		// Remove double blank lines after the package or last import
+
+		newContent = newContent.replaceFirst(
+			"(?m)^[ \t]*((?:package|import) .*;)\\s*^[ \t]*/\\*\\*",
+			"$1\n\n/**");
+
+		/*// Remove blank lines after try {
+
+		newContent = StringUtil.replace(newContent, "try {\n\n", "try {\n");
+
+		// Remove blank lines after ) {
+
+		newContent = StringUtil.replace(newContent, ") {\n\n", ") {\n");
+
+		// Remove blank lines empty braces { }
+
+		newContent = StringUtil.replace(newContent, "\n\n\t}", "\n\t}");
+
+		// Add space to last }
+
+		newContent =
+			newContent.substring(0, newContent.length() - 2) + "\n\n}";*/
+
+		ToolsUtil.writeFileRaw(file, newContent, modifiedFileNames);
+
+		if (failOnFormatError && !formatSuccess) {
+			throw new IOException("Unable to beautify " + file);
+		}
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
