9ae81e3be1ee5eeb7145a682a17a857c2926e0ba
Kevin Lee <kevin.lee@liferay.com>
2025-05-15 11:02:04 -0700

    LPD-55687 (TEST; DO NOT MERGE) Add dummy unit tests to check if relevant will catch and run these tests

---
+++ modules/apps/portlet-dependency-factory/portlet-dependency-factory-impl/src/test/java/com/liferay/portlet/dependency/factory/internal/DummyTest.java
@@ -0,0 +1,30 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portlet.dependency.factory.internal;
+
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Kevin Lee
+ */
+public class DummyTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Test
+	public void testDummy() {
+		Assert.assertTrue("Uh oh", 1 == 1);
+	}
+
+}
