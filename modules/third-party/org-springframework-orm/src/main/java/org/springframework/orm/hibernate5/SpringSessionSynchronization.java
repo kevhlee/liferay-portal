eb3a44bac00bedd575e3034259c27fd53a673509
Kevin Lee <kevin.lee@liferay.com>
2025-08-18 16:49:37 -0700

    Revert "LPD-61104 spring-orm: Re-patch LPS-100343"
    This reverts commit 696ece2cff568d15c53188bde4d9bf3584d73ee9.

--- modules/third-party/org-springframework-orm/src/main/java/org/springframework/orm/hibernate5/SpringSessionSynchronization.java
+++ modules/third-party/org-springframework-orm/src/main/java/org/springframework/orm/hibernate5/SpringSessionSynchronization.java
@@ -69,6 +69,11 @@ public class SpringSessionSynchronization implements TransactionSynchronization,
 	public void suspend() {
 		if (this.holderActive) {
 			TransactionSynchronizationManager.unbindResource(this.sessionFactory);
+			// Eagerly disconnect the Session here, to make release mode "on_close" work on JBoss.
+			Session session = getCurrentSession();
+			if (session instanceof SessionImplementor sessionImpl) {
+				sessionImpl.getJdbcCoordinator().getLogicalConnection().manualDisconnect();
+			}
 		}
 	}
 
@@ -141,4 +146,3 @@ public class SpringSessionSynchronization implements TransactionSynchronization,
 	}
 
 }
-/* @generated */
