a3ee7c8b2cdfdfc6fb71defee2dd4e0df3be5475
Kevin Lee <kevin.lee@liferay.com>
2026-03-30 12:28:07 -0700

    LPD-80367 Add test to make sure Setup Wizard does not display when an invalid license is deployed

--- modules/apps/portal/portal-license-test/src/testIntegration/java/com/liferay/portal/license/test/EnterpriseDatabaseTest.java
+++ modules/apps/portal/portal-license-test/src/testIntegration/java/com/liferay/portal/license/test/EnterpriseDatabaseTest.java
@@ -6,14 +6,20 @@
 package com.liferay.portal.license.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.db.DB;
 import com.liferay.portal.kernel.dao.db.DBManagerUtil;
 import com.liferay.portal.kernel.dao.db.DBType;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.AssumeTestRule;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.PropsValues;
+import com.liferay.portal.test.log.LogCapture;
+import com.liferay.portal.test.log.LoggerTestUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.Collections;
 
 import net.bytebuddy.agent.builder.ResettableClassFileTransformer;
 
@@ -94,6 +100,42 @@ public class EnterpriseDatabaseTest extends BaseLicenseTestCase {
 		for (DBType dbType : _DB_TYPES) {
 			Assert.assertTrue(
 				response.contains("value=\"" + dbType.getName() + "\""));
+		}
+	}
+
+	@Test
+	public void testInvalidLicenseSetupWizard() throws Exception {
+		Assume.assumeTrue(PropsValues.SETUP_WIZARD_ENABLED);
+
+		assertPortalLicenseNotRegistered();
+
+		deployFreeTierPortalLicense();
+
+		assertPortalLicenseRegistered();
+
+		String response = hitHomePage("localhost", 8080);
+
+		Assert.assertTrue(response.contains("setup_wizard"));
+
+		resetLicenseData();
+
+		assertPortalLicenseNotRegistered();
+
+		try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+				getProperty("license.manager.class.name"),
+				LoggerTestUtil.ERROR)) {
+
+			deployFreeTierPortalLicense(
+				Collections.emptyList(), StringPool.BLANK,
+				System.currentTimeMillis());
+
+			assertPortalLicenseInvalid();
+
+			Assert.assertFalse(ListUtil.isEmpty(logCapture.getMessages()));
+
+			response = hitHomePage("localhost", 8080);
+
+			Assert.assertFalse(response.contains("setup_wizard"));
 		}
 	}
 
