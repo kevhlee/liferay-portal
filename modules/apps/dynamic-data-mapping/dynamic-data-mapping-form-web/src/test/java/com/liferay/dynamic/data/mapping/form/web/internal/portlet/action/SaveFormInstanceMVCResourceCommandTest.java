779d3095acda68b30f571966163cd87474036187
Kevin Lee <kevin.lee@liferay.com>
2026-07-20 20:07:25 -0700

    LPD-61080 Remove Java.isJDK*() methods and their usages

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/test/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/SaveFormInstanceMVCResourceCommandTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/test/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/SaveFormInstanceMVCResourceCommandTest.java
@@ -5,7 +5,6 @@
 
 package com.liferay.dynamic.data.mapping.form.web.internal.portlet.action;
 
-import com.liferay.portal.kernel.util.JavaDetector;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
@@ -46,26 +45,14 @@ public class SaveFormInstanceMVCResourceCommandTest {
 
 		Date date = calendar.getTime();
 
-		if (!JavaDetector.isJDK8()) {
-			Assert.assertEquals(
-				"Apr 18, 2018, 11:00 AM",
-				_saveFormInstanceMVCResourceCommand.formatDate(
-					date, LocaleUtil.US, "America/Sao_Paulo"));
-			Assert.assertEquals(
-				"Apr 18, 2018, 2:00 PM",
-				_saveFormInstanceMVCResourceCommand.formatDate(
-					date, LocaleUtil.US, "UTC"));
-		}
-		else {
-			Assert.assertEquals(
-				"Apr 18, 2018 11:00 AM",
-				_saveFormInstanceMVCResourceCommand.formatDate(
-					date, LocaleUtil.US, "America/Sao_Paulo"));
-			Assert.assertEquals(
-				"Apr 18, 2018 2:00 PM",
-				_saveFormInstanceMVCResourceCommand.formatDate(
-					date, LocaleUtil.US, "UTC"));
-		}
+		Assert.assertEquals(
+			"Apr 18, 2018, 11:00 AM",
+			_saveFormInstanceMVCResourceCommand.formatDate(
+				date, LocaleUtil.US, "America/Sao_Paulo"));
+		Assert.assertEquals(
+			"Apr 18, 2018, 2:00 PM",
+			_saveFormInstanceMVCResourceCommand.formatDate(
+				date, LocaleUtil.US, "UTC"));
 	}
 
 	private void _setUpSaveFormInstanceMVCResourceCommand() throws Exception {
--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceTest.java
@@ -220,7 +220,6 @@ import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
-import com.liferay.portal.kernel.util.JavaDetector;
 import com.liferay.portal.kernel.util.LinkedHashMapBuilder;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleThreadLocal;
@@ -2187,7 +2186,7 @@ public class ObjectEntryLocalServiceTest {
 						EncryptorException.class.getName(), ": ",
 						EncryptorException.class.getName(), ": ",
 						NoSuchAlgorithmException.class.getName(),
-						_getNoSuchAlgorithmExceptionMessage()),
+						": Null or empty transformation"),
 					() -> _objectEntryLocalService.getValues(
 						objectEntry.getObjectEntryId()));
 
@@ -2197,7 +2196,7 @@ public class ObjectEntryLocalServiceTest {
 						EncryptorException.class.getName(), ": ",
 						EncryptorException.class.getName(), ": ",
 						NoSuchAlgorithmException.class.getName(),
-						_getNoSuchAlgorithmExceptionMessage()),
+						": Null or empty transformation"),
 					() -> _addObjectEntry(
 						HashMapBuilder.<String, Serializable>put(
 							"emailAddress", RandomTestUtil.randomString()
@@ -9135,14 +9134,6 @@ public class ObjectEntryLocalServiceTest {
 		}
 
 		return sb.toString();
-	}
-
-	private String _getNoSuchAlgorithmExceptionMessage() {
-		if (JavaDetector.isJDK21()) {
-			return ": Null or empty transformation";
-		}
-
-		return ": Invalid transformation format:";
 	}
 
 	private String _getRandomEmailAddress() {
