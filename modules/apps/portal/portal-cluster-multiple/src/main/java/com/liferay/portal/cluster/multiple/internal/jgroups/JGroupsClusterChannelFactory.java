d7e671be07bd8b4fff85a9730e965c4e40e5ff40
Kevin Lee <kevin.lee@liferay.com>
2026-09-14 17:14:28 -0700

    LPD-87976 Replace the default MD5 implementation with SYM_ENCRYPT
    JGroups 5 removed the MD5Token AUTH implementation as attackers could
    find out the key through replay attacks and bypass AUTH. ASYM_ENCRYPT on
    its own only provides message encryption and has no gating mechanism to
    prevent random nodes from joining the cluster. Thus, this combination
    cannot be kept.
    
    The SYM_ENCRYPT encryption protocol can be used to close the security
    gap covered by this combination. A protocol hook is used to set the
    key for each channel. A node that cannot derive each channel's key
    correctly has its join request rejected by the encryption layer,
    preventing it from joining the cluster. Additionally, the key is never
    transmitted over the network.
    
    As recommended by the JGroups manual, the SERIALIZE protocol is placed
    on top of SYM_ENCRYPT to serialize the entire message and further guard
    against replay attacks.
    
    It should be noted that the secure/x509 implementation that Liferay
    ships is more secure than the secure/sym_encrypt implementation.
    Deployments should consider using secure/x509 if stronger security is a
    concern.

--- modules/apps/portal/portal-cluster-multiple/src/main/java/com/liferay/portal/cluster/multiple/internal/jgroups/JGroupsClusterChannelFactory.java
+++ modules/apps/portal/portal-cluster-multiple/src/main/java/com/liferay/portal/cluster/multiple/internal/jgroups/JGroupsClusterChannelFactory.java
@@ -34,10 +34,13 @@ import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutorService;
 
 import org.jgroups.conf.ConfiguratorFactory;
+import org.jgroups.conf.ProtocolConfiguration;
 import org.jgroups.conf.ProtocolStackConfigurator;
 
 /**
@@ -66,10 +69,26 @@ public class JGroupsClusterChannelFactory implements ClusterChannelFactory {
 		ClusterReceiver clusterReceiver) {
 
 		try {
+			ProtocolStackConfigurator protocolStackConfigurator =
+				_parseChannelProperties(channelPropertiesLocation);
+
+			for (ProtocolConfiguration protocolConfiguration :
+					protocolStackConfigurator.getProtocolStack()) {
+
+				Map<String, String> properties =
+					protocolConfiguration.getProperties();
+
+				if (Objects.equals(
+						properties.get("after_creation_hook"),
+						SymEncryptProtocolHook.class.getName())) {
+
+					properties.put("alias", clusterName);
+				}
+			}
+
 			return new JGroupsClusterChannel(
-				executorService, channleLogicName,
-				_parseChannelProperties(channelPropertiesLocation), clusterName,
-				clusterReceiver, _bindInetAddress,
+				executorService, channleLogicName, protocolStackConfigurator,
+				clusterName, clusterReceiver, _bindInetAddress,
 				_clusterExecutorConfiguration, _classLoaders);
 		}
 		catch (Exception exception) {
@@ -198,18 +217,19 @@ public class JGroupsClusterChannelFactory implements ClusterChannelFactory {
 			String channelPropertiesLocation)
 		throws Exception {
 
-		if (channelPropertiesLocation.startsWith("jgroups/secure/md5/") &&
-			_log.isWarnEnabled() && _defaultMD5Warning) {
+		if (channelPropertiesLocation.startsWith(
+				"jgroups/secure/sym_encrypt/") &&
+			_log.isWarnEnabled() && _defaultSymEncryptWarning) {
 
 			_log.warn(
 				StringBundler.concat(
-					"Clustering authentication is using MD5 default ",
+					"Clustering authentication is using SYM_ENCRYPT default ",
 					"implementation. Please note that this implementation is ",
 					"not secure enough to be used in production. Refer to the ",
 					"documentation for details on configuring secure JGroups ",
 					"connections."));
 
-			_defaultMD5Warning = false;
+			_defaultSymEncryptWarning = false;
 		}
 
 		try (InputStream inputStream = _getInputStream(
@@ -300,8 +320,8 @@ public class JGroupsClusterChannelFactory implements ClusterChannelFactory {
 	private static final Log _log = LogFactoryUtil.getLog(
 		JGroupsClusterChannelFactory.class);
 
-	private static boolean _defaultMD5Warning = true;
 	private static boolean _defaultSecretWarning = true;
+	private static boolean _defaultSymEncryptWarning = true;
 
 	private InetAddress _bindInetAddress;
 	private NetworkInterface _bindNetworkInterface;
---
+++ modules/apps/portal/portal-cluster-multiple/src/main/java/com/liferay/portal/cluster/multiple/internal/jgroups/SymEncryptProtocolHook.java
@@ -0,0 +1,34 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.cluster.multiple.internal.jgroups;
+
+import com.liferay.portal.kernel.util.DigesterUtil;
+
+import javax.crypto.spec.SecretKeySpec;
+
+import org.jgroups.protocols.SYM_ENCRYPT;
+import org.jgroups.stack.Protocol;
+import org.jgroups.stack.ProtocolHook;
+
+/**
+ * @author Kevin Lee
+ */
+public class SymEncryptProtocolHook implements ProtocolHook {
+
+	@Override
+	public void afterCreation(Protocol protocol) {
+		if (!(protocol instanceof SYM_ENCRYPT symEncrypt)) {
+			return;
+		}
+
+		symEncrypt.setSecretKey(
+			new SecretKeySpec(
+				DigesterUtil.digestRaw(
+					DigesterUtil.SHA_256, symEncrypt.alias()),
+				"AES"));
+	}
+
+}
